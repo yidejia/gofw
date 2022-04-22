e835f41692e157200ce4cffc4f895e0cb99fbbf2
余海坚 <yuhaijian@ydjdev.com>
2022-04-22 18:20:28 +0800

    发送邮件验证码

--- verifycode/verifycode.go
+++ verifycode/verifycode.go
@@ -2,10 +2,12 @@
 package verifycode
 
 import (
+	"fmt"
 	"github.com/yidejia/gofw/app"
 	"github.com/yidejia/gofw/config"
 	"github.com/yidejia/gofw/helpers"
 	"github.com/yidejia/gofw/logger"
+	"github.com/yidejia/gofw/mail"
 	"github.com/yidejia/gofw/redis"
 	"github.com/yidejia/gofw/sms"
 	"strings"
@@ -88,4 +90,31 @@ func (vc *VerifyCode) generateVerifyCode(key string) string {
 	// 将验证码及 KEY（邮箱或手机号）存放到 Redis 中并设置过期时间
 	vc.Store.Set(key, code)
 	return code
+}
+
+// SendEmail 发送邮件验证码，调用示例：
+//         verifycode.NewVerifyCode().SendEmail(request.Email)
+func (vc *VerifyCode) SendEmail(email string) error {
+
+	// 生成验证码
+	code := vc.generateVerifyCode(email)
+
+	// 方便本地和 API 自动测试
+	if !app.IsProduction() && strings.HasSuffix(email, config.GetString("verifycode.debug_email_suffix")) {
+		return nil
+	}
+
+	content := fmt.Sprintf("<h1>您的 Email 验证码是 %v </h1>", code)
+	// 发送邮件
+	mail.NewMailer().Send(mail.Email{
+		From: mail.From{
+			Address: config.GetString("mail.from.address"),
+			Name:    config.GetString("mail.from.name"),
+		},
+		To:      []string{email},
+		Subject: "Email 验证码",
+		HTML:    []byte(content),
+	})
+
+	return nil
 }
