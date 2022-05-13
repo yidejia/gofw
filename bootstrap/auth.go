ab9d4703006b6d4994584fa1efb8ff640ee212bd
余海坚 <yuhaijian@ydjdev.com>
2022-05-13 15:30:39 +0800

    完善 SetupAuth 初始化授权注释

--- bootstrap/auth.go
+++ bootstrap/auth.go
@@ -9,7 +9,7 @@ import (
 // SetupAuth 初始化授权
 func SetupAuth()  {
 	auth.SetUserResolver(func(userId string) (user auth.Authenticate, err gfErrors.ResponsiveError) {
-		// TODO 这里获取用户模型的逻辑，根据应用需要进行修改
+		// TODO 这里实现根据用户 id 获取用户模型的逻辑，根据应用需要进行修改
 		_user, err := userSvcs.NewUserService().Get(userId)
 		return &_user, err
 	})
